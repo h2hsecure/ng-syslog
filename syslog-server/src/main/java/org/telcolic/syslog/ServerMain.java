e1e53db9d5b1e61953e799e1c38a2a5e54658295
hekemen <huseyinekemen@gmail.com>
2016-05-12 01:02:08 +0300

    Inintial commit

---
+++ syslog-server/src/main/java/org/telcolic/syslog/ServerMain.java
@@ -0,0 +1,16 @@
+package org.telcolic.syslog;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by hekemen on 5/12/16.
+ */
+public class ServerMain {
+    private static Logger log = LoggerFactory.getLogger(ServerMain.class);
+
+
+    public static void main(String args[]) {
+        log.info("test");
+    }
+}
